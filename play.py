269433f836acb04a9308a5e42b7c762401d6cef7
Yvon Cognard <yvon@cognard.com>
2021-07-10 22:54:32 +0200

    Initial commit

---
+++ play.py
@@ -0,0 +1,39 @@
+#!python3
+
+import os
+from ctypes import windll
+import tkinter as tk
+from tkinter import filedialog
+
+windll.shcore.SetProcessDpiAwareness(1)
+
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askopenfilename(
+		initialdir=os.getcwd(),
+		title="Select file",
+		filetypes=[("Audio Files", "*.wav")]
+)
+
+import sounddevice as sd
+import soundfile as sf
+
+# Extract data and sampling rate from file
+data, fs = sf.read(file_path, dtype='float32')  
+sd.play(data, fs)
+
+import sys
+import re
+
+search = re.search('record-\d+-(\d+)-(.+)\.wav$', file_path)
+session_num = int(search.group(1))
+session_time_ms = int(float(search.group(2)) * 1000) # in ms
+
+import irsdk
+ir = irsdk.IRSDK()
+ir.startup()
+
+ir.replay_search_session_time(session_num, session_time_ms)
+
+status = sd.wait()  # Wait until file is done playing
---
+++ record.py
@@ -0,0 +1,35 @@
+#!python3
+
+import irsdk
+ir = irsdk.IRSDK()
+ir.startup()
+
+filename = 'record-%s-%s-%s.wav' % (ir['SessionUniqueID'], ir['SessionNum'], ir['SessionTime'])
+print('Recording: ' + filename)
+
+import queue
+import sounddevice as sd
+import soundfile as sf
+import numpy  # Make sure NumPy is loaded before it is used in the callback
+assert numpy  # avoid "imported but unused" message (W0611)
+
+samplerate = 44100  # Sample rate
+seconds = 60  # Duration of recording
+
+q = queue.Queue()
+
+def callback(indata, frames, time, status):
+    """This is called (from a separate thread) for each audio block."""
+    if status:
+        print(status, file=sys.stderr)
+    q.put(indata.copy())
+
+try:
+    with sf.SoundFile(filename, mode='x', samplerate=samplerate, channels=1) as file:
+        with sd.InputStream(samplerate=samplerate, callback=callback, channels=1):
+            print('Press Ctrl+C to stop the recording')
+            while True:
+                file.write(q.get())
+except KeyboardInterrupt:
+    print('Finished')
+    exit(0)
